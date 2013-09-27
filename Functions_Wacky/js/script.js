725471762a884e6caf9e515266f8252d31482d0a
dbutcher93 <dbutcher93@fullsail.edu>
2013-09-27 02:00:48 -0400

    Functions
    Competed Wacky Subproject

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,12 +1,13 @@
-console.log("-----Functions: Wacky-----")
+console.log("-----Functions: Wacky-----") // this shows that it is the Wacky subproject
 
-var skateboardBails = function(){
-	var skaters = 5;
-	var bailsperHour = 10;
+var skateboardBails = function(){ //this is the anonymous function
+	var skaters = 5; //this is how many skaters are in the skatepark
+	var bailsperHour = 10; // this is how many times they bail per hour
+	var hours = 3; //this is how many total hours at the skatepark
 
-	var totalBails = skaters * bailsperHour;
+	var totalBails = skaters * bailsperHour * 3; //this is how many total bails
 
-	console.log(totalBails)
+	console.log(totalBails) //this tells the answer
 }
 
-skateboardBails();
+skateboardBails(); //this invokes the function
