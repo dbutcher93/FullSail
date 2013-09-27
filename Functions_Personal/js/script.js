cdcf1a2d6c845d47905214804e8d6fbe4a0ae295
dbutcher93 <dbutcher93@fullsail.edu>
2013-09-26 23:05:17 -0400

    Functions
    this is the complete personal subproject

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,10 @@
-console.log ("-----Functions: Personal-----")
+console.log ("-----Functions: Personal-----") // this indicates that it is the personal assignment
+
+carLease(20, 316); // these are the function argumemnts
+
+function carLease(monthsLeft, Payment) { //these are the function parameters  and the fuction
+	var amountleft = monthsLeft * Payment; //this it a variable defining the amount owed on the lease
+	console.log("You still owe " + amountleft + " dollars on your lease") // this shows the answer to the console
+}
+
+carLease(); // this invokes the function, which it invokes twice and I can not figure out why
