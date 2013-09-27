741d749e8dda4a46eae1217d49dfbfcc2281b9a0
dbutcher93 <dbutcher93@fullsail.edu>
2013-09-27 01:57:20 -0400

    Functions

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,10 +1,12 @@
 console.log("-----Functions: Wacky-----")
 
 var skateboardBails = function(){
-	var skaters = 2;
-	var bailsperHour = 10;
+	var skaters = 3;
+	var bailsperHour = 15;
 
 	var totalBails = skaters * bailsperHour;
 
 	console.log(totalBails)
 }
+
+skateboardBails();
