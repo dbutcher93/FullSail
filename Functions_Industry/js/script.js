5b12600a4adb10d6f4fba87f9cad0a2ac8c45a5c
dbutcher93 <dbutcher93@fullsail.edu>
2013-09-27 00:59:01 -0400

    Functions

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,11 +1,20 @@
 console.log("-----Functions: Industry-----")
 
-lines(20, 13, 56);
+orders(25, 13, 60);
 
-function lines(order1, order2, order3){
-	var linesCompleted = order1 + order2 + order3;
+function orders (order1, order2, order3){
+	var totalLines = order1 + order2 + order3;
+	var linesCompleted = 72;
+	var linesLeft = totalLines - linesCompleted;
+
+if (totalLines == linesCompleted){
+	console.log("You have completed all of the lines on the orders")
+} else if (linesLeft < 10 ){
+	console.log("You have less than 10 lines left")
+} else {
+	console.log("You have " + linesLeft + " orders left")
+}
 
 }
 
-var totalLines = 96;
 
